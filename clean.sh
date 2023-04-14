b75a704f686b5de8cc7627c44458aa5cdd04a21d
t-ESC <the.krinitz@gmail.com>
2023-04-14 19:29:26 +0200

    first commit

---
+++ clean.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+rm *.for
+rm *.loe
+rm *.bak
+rm *.aux
+rm *.bbl
+rm *.blg
+rm *.log
+rm *.bcf
+rm *.glo
+rm *.ist
+rm *.lof
+rm *.lol
+rm *.lot
+rm *.toc
+rm *.tps
+rm *.run.xml
+rm *.synctex.gz
+rm template-blx.bib
